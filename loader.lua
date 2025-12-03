ea19be0a507d4d0a4b20df637249523d6be9446a
Miwowa1337 <miwow@inboxa.de>
2025-12-02 22:35:10 -0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -10,7 +10,7 @@ if not game:IsLoaded() then game.Loaded:Wait() end
 
 ---//
 if game.PlaceId == 2534724415 then
-    loadstring(game:HttpGet("https://erlchook.loaders.csphere.vip"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CheatSphere/ERLChook/refs/heads/main/script/ERLChook.lua"))()
 else
     game.StarterGui:SetCore("SendNotification",{Title = "ERLChook",Text = "ERLChook doesn't work on other games than ER:LC!",Duration = 10,})
 end
