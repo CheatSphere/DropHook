cd63b1eb0b25a981e1dda4cbab612ccbefeefacd
Miwowa1337 <miwow@inboxa.de>
2025-12-10 13:49:22 -0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,7 +4,7 @@ local IsMobile = game:GetService("UserInputService"):GetPlatform() == Enum.Platf
 
 if IsMobile and not getgenv().MobileYes then
     getgenv().MobileYes = true
-    game.StarterGui:SetCore("SendNotification", {Title = "ERLChook",Text = "The script isn't tested on mobile and might be buggy, if you still want to use ERLChook just run the scritp again.",Duration = 15,})
+    game.StarterGui:SetCore("SendNotification", {Title = "DropHook",Text = "The script isn't tested on mobile and might be buggy, if you still want to use DropHook just run the scritp again.",Duration = 15,})
     return
 end
 
@@ -18,23 +18,23 @@ then
 
 if not game:IsLoaded() then game.Loaded:Wait() end
 
-if not isfolder("ERLChook") then makefolder("ERLChook") end
-if not isfolder("ERLChook/configs") then makefolder("ERLChook/configs") end
-if not isfolder("ERLChook/settings") then makefolder("ERLChook/settings") end
-if not isfolder("ERLChook/assets") then makefolder("ERLChook/assets") end
+if not isfolder("DropHook") then makefolder("DropHook") end
+if not isfolder("DropHook/configs") then makefolder("DropHook/configs") end
+if not isfolder("DropHook/settings") then makefolder("DropHook/settings") end
+if not isfolder("DropHook/assets") then makefolder("DropHook/assets") end
 
 --// is already loaded
-if getgenv().ERLChookloaded then 
-    game.StarterGui:SetCore("SendNotification", {Title = "ERLChook",Text = "The script is already loaded!",Duration = 5,})
+if getgenv().DropHookloaded then 
+    game.StarterGui:SetCore("SendNotification", {Title = "DropHook",Text = "The script is already loaded!",Duration = 5,})
     return
 end
 --\\
 
 ---// game check
 if game.PlaceId == 2534724415 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CheatSphere/ERLChook/refs/heads/main/script/ERLChook.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CheatSphere/DropHook/refs/heads/main/script/DropHook.lua"))()
 else
-    game.StarterGui:SetCore("SendNotification",{Title = "ERLChook",Text = "ERLChook doesn't work on other games than ER:LC!",Duration = 10,})
+    game.StarterGui:SetCore("SendNotification",{Title = "DropHook",Text = "DropHook doesn't work on other games than ER:LC!",Duration = 10,})
 end
 ---\\
 
@@ -42,7 +42,7 @@ end
 ------
 
 else
-game.StarterGui:SetCore("SendNotification",{Title = "ERLChook",Text = "Executor not supported!",Duration = 10,})
+game.StarterGui:SetCore("SendNotification",{Title = "DropHook",Text = "Executor not supported!",Duration = 10,})
 end
 
 ---\\
