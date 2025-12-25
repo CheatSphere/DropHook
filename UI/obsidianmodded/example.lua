7f700e91bd865505ea8446e7c2885e1c62e47368
Miwowa1337 <miwow@inboxa.de>
2025-12-25 01:42:38 -0500

    Update example.lua

--- UI/obsidianmodded/example.lua
+++ UI/obsidianmodded/example.lua
@@ -19,11 +19,13 @@ local Tabs = {
 	["UI Settings"] = Window:AddTab("UI Settings", "settings"),
 }
 
-local LeftGroupBox = Tabs.Main:AddLeftGroupbox("Groupbox", "boxes")
-local DropdownGroupBox = Tabs.Main:AddRightGroupbox("Dropdowns", "boxes")
+local groupboxes = {
+    LeftGroupBox = Tabs.Main:AddLeftGroupbox("Groupbox", "boxes"),
+    DropdownGroupBox = Tabs.Main:AddRightGroupbox("Dropdowns", "boxes")
+}
 
 
-LeftGroupBox:AddToggle("MyToggle", {Text = "This is a toggle",Default = true,Callback = function(Value)
+groupboxes.LeftGroupBox:AddToggle("MyToggle", {Text = "This is a toggle",Default = true,Callback = function(Value)
 		print("[cb] MyToggle changed to:", Value)
 end})
 	:AddColorPicker("ColorPicker1", {Default = Color3.fromRGB(255,255,255),Title = "colorpicker",--Transparency = 0,
@@ -31,25 +33,25 @@ end})
 		print("[cb] Color changed!", Value)
 end})
 
-LeftGroupBox:AddButton("button", function() 
+groupboxes.LeftGroupBox:AddButton("button", function() 
     print("ok")
 end)
 
-LeftGroupBox:AddLabel("This is a label")
+groupboxes.LeftGroupBox:AddLabel("This is a label")
 
-LeftGroupBox:AddLabel("This is a label\n\nwhich wraps its text!", true)
+groupboxes.LeftGroupBox:AddLabel("This is a label\n\nwhich wraps its text!", true)
 
-LeftGroupBox:AddSlider("FOVsize", {Text = "FOV Size",Default = 125,Min = 1,Max = 250,Rounding = 0,Callback = function(Value)
+groupboxes.LeftGroupBox:AddSlider("FOVsize", {Text = "FOV Size",Default = 125,Min = 1,Max = 250,Rounding = 0,Callback = function(Value)
     print("[cb] MySlider was changed! New value:", Value)
 end})
 
-LeftGroupBox:AddInput("MyTextbox", {Default = "My textbox!",Numeric = false,Finished = false,
+groupboxes.LeftGroupBox:AddInput("MyTextbox", {Default = "My textbox!",Numeric = false,Finished = false,
 	Text = "This is a textbox",
 	Callback = function(Value)
 		print("[cb] Text updated. New text:", Value)
 end})
 
-DropdownGroupBox:AddDropdown("dropdown", {Values = { "1", "2" },Default = "1",Text = "dropdown",Searchable = true,Callback = function(Value) --// multi true
+groupboxes.DropdownGroupBox:AddDropdown("dropdown", {Values = { "1", "2" },Default = "1",Text = "dropdown",Searchable = true,Callback = function(Value) --// multi true
 		print("[cb] Dropdown got changed. New value:", Value)
 end})
 
